a5e325f9ee63491304bd690cb82e391ed4dbcb35
bitfl0wer <florian@pro-weber.com>
2025-01-26 14:43:12 +0100

    add configuration.nix module

--- hosts/snowstorm/configuration.nix
+++ hosts/snowstorm/configuration.nix
@@ -4,34 +4,18 @@
 
   imports = [
     inputs.home-manager.nixosModules.home-manager
-    ../../modules/users.nix
-    ../../modules/packages.nix
+    ../../modules/locale.nix
     ../../modules/networking.nix
+    ../../modules/packages.nix
+    ../../modules/openssh.nix
     ../../modules/docker.nix
+    ../../modules/users.nix
   ];
 
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/vda";
   boot.loader.grub.useOSProber = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
 
   programs.zsh.enable = true;
 
@@ -43,11 +27,6 @@
   #   enableSSHSupport = true;
   # };
 
-  services.openssh = {
-    enable = true;
-    settings.PasswordAuthentication = false;
-    settings.KbdInteractiveAuthentication = false;
-  };
   # Open ports in the firewall. networking.firewall.allowedTCPPorts = [ ... ]; networking.firewall.allowedUDPPorts = [ 
   # ... ]; Or disable the firewall altogether. networking.firewall.enable = false;
 
