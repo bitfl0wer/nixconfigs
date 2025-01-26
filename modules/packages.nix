be32c435206a54dd5938cec78b7f1df0df550eb8
bitfl0wer <florian@pro-weber.com>
2025-01-26 14:52:49 +0100

    add fail2ban config

--- modules/packages.nix
+++ modules/packages.nix
@@ -23,4 +23,19 @@
     zoxide
     gh
   ];
+  programs.zsh.enable = true;
+  # NixOS comes with a pre-configured SSH jail which will observe errors in the SSH daemon and ban offending IPs.
+  services.fail2ban = {
+    enable = true;
+    maxretry = 5;
+    ignoreIP = [ "192.168.172.0/16" ];
+    bantime = "6h";
+    bantime-increment = {
+      enable = true;
+      formula =
+        "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+      maxtime = "168h";
+      overalljails = true;
+    };
+  };
 }
