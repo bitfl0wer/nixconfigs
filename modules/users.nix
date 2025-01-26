bf12251b6759a599fe7b3f38d547ffebf73e45d9
bitfl0wer <florian@pro-weber.com>
2025-01-26 14:34:19 +0100

    define two users: flori and viv, based on their identities

--- modules/users.nix
+++ modules/users.nix
@@ -1,5 +1,25 @@
-{ ... }:
+{ identities, pkgs, ... }:
 
 {
+  users.mutableUsers = false;
+  users.defaultUserShell = pkgs.zsh;
 
+  users.users = {
+    ${identities.flori.username} = {
+      initialHashedPassword = identities.flori.password;
+      openssh.authorizedKeys.keys = identities.flori.pubkeys;
+      useDefaultShell = true;
+      isNormalUser = true;
+      createHome = true;
+      extraGroups = [ "wheel" ];
+    };
+    ${identities.viv.username} = {
+      initialHashedPassword = identities.viv.password;
+      openssh.authorizedKeys.keys = identities.viv.pubkeys;
+      useDefaultShell = true;
+      isNormalUser = true;
+      createHome = true;
+      extraGroups = [ "wheel" ];
+    };
+  };
 }
