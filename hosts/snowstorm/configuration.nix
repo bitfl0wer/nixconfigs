d892f158bd732dcb2850fde8cea0037d1d8fbca1
bitfl0wer <florian@pro-weber.com>
2025-01-26 14:52:43 +0100

    move zsh.enable to packages.nix

--- hosts/snowstorm/configuration.nix
+++ hosts/snowstorm/configuration.nix
@@ -17,8 +17,6 @@
   boot.loader.grub.device = "/dev/vda";
   boot.loader.grub.useOSProber = true;
 
-  programs.zsh.enable = true;
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
