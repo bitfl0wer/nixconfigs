6244bbb0b7d13ea10d172c9b36d8d9031e4df023
bitfl0wer <florian@pro-weber.com>
2025-01-26 14:33:23 +0100

    create identities "flori" and "viv"

---
+++ identities/flori.nix
@@ -0,0 +1,9 @@
+{
+  pubkeys = {
+    star_ssh =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBnstd9HyyIjfXWfGymWDcRlK9nZuqgTIcueiqPUDaQ star@starforge";
+  };
+  password =
+    "$argon2id$v=19$m=16000,t=4,p=8$ZmQyNDU0ZTBlNzRmMTU3MzQ0NGNkNzRlMzJkNDIzM2NiYWZlYzk3ZjcxZTEwOTI4ZWE1OGJlZDUwMjA0NzI2NA$u36BRZGQrSfIAMdFmG3wDMbajxHe6+jps7vblpuUwaM";
+  username = "star";
+}
---
+++ identities/viv.nix
@@ -0,0 +1,6 @@
+{
+  pubkeys = { };
+  password =
+    "$argon2id$v=19$m=16000,t=4,p=8$NjQ4OWJhMTE5NmQ5NjAyYTk4MDEzYjZlZDQ3NDlmZmMzMjU4MWMxYmFkMmFlYWZjMDM1MzI5MGJhY2JjYTg4Mg$Bf0gW3ol4cZOhYkf2jU5nrDkXJ1/dEbJoFw4rOIJZck";
+  username = "viv";
+}
